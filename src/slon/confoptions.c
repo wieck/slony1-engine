e4285eba5740dfe535925af232086e0ab3d0077b
Jan Wieck <JanWieck@Yahoo.com>
2013-07-31 11:51:14 -0400

    Bug 303 - local node lock retry on startup
    Add configuration options for retry count and interval
    of acquiring the local node lock on startup.

--- src/slon/confoptions.c
+++ src/slon/confoptions.c
@@ -204,6 +204,32 @@ static struct config_int ConfigureNamesInt[] =
 	},
 	{
 		{
+			(const char *) "startup_node_lock_retry_count",
+			gettext_noop("How often to retry to acquire the local node lock "
+						 "on initial startup. Default = 2."),
+			NULL,
+			SLON_C_INT,
+		},
+		&startup_node_lock_retry_count,
+		2,
+		0,						/* min val */
+		1073741824				/* max val */
+	},
+	{
+		{
+			(const char *) "startup_node_lock_retry_interval",
+			gettext_noop("How fast to retry to acquire the local node lock "
+						 "on startup or retry in seconds. Default = 5."),
+			NULL,
+			SLON_C_INT,
+		},
+		&startup_node_lock_retry_interval,
+		5,
+		1,						/* min val */
+		1073741824				/* max val */
+	},
+	{
+		{
 			(const char *) "apply_cache_size",
 			gettext_noop("apply cache size"),
 			gettext_noop("apply cache size in number of prepared queries"),
